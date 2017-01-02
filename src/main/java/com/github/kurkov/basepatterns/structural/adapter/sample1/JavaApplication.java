cb25a3331643fd3eb05437d2b102c06d5945a028
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-01-02 15:43:09 +0200

    Refactor package name

--- src/main/java/com/github/kurkov/basepatterns/structural/adapter/sample1/JavaApplication.java
+++ src/main/java/com/github/kurkov/basepatterns/structural/adapter/sample1/JavaApplication.java
@@ -1,4 +1,4 @@
-package com.github.kurkov.basepatterns.structural.adapter.sample;
+package com.github.kurkov.basepatterns.structural.adapter.sample1;
 
 /**
  * @author Aleksey Kurkov. Created on 02.11.2016
