cca6f4273e77aae98f848dd3f4211a85864128b5
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2017-02-27 16:28:01 +0200

    Refactor package name

--- src/main/java/com/github/kurkov/basepatterns/structural/facade/sample1/Workflow.java
+++ src/main/java/com/github/kurkov/basepatterns/structural/facade/sample1/Workflow.java
@@ -1,4 +1,4 @@
-package com.github.kurkov.basepatterns.structural.facade.sample;
+package com.github.kurkov.basepatterns.structural.facade.sample1;
 
 /**
  * @author Aleksey Kurkov. Created on 09.11.2016
